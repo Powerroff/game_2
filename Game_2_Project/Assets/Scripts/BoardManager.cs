ccc2cb07e4ebe0bebdac9a63fa55b9f524975b01
Powerroff <aaron36berger@gmail.com>
2020-07-14 22:46:19 -0400

    7/14 - Tweaks
    Camera tweak, biome gen tweak

--- Game_2_Project/Assets/Scripts/BoardManager.cs
+++ Game_2_Project/Assets/Scripts/BoardManager.cs
@@ -33,7 +33,7 @@ public class BoardManager : MonoBehaviour
         List<Biome> biomes;
         public void init() {
             biomes = new List<Biome>();
-            for (int i = 0; i < 12; i++) {
+            for (int i = 0; i < 6; i++) {
                 biomes.Add(Biome.random());
             }
         }
@@ -55,17 +55,21 @@ public class BoardManager : MonoBehaviour
     {
         Vector2 center;
         public int label;
+        float scale;
+        int waves;
 
         public static Biome random() {
             Biome b = new Biome();
             b.center = new Vector2(UnityEngine.Random.Range(-30, 30), UnityEngine.Random.Range(-30, 30));
             b.label = UnityEngine.Random.Range(0, 100);
+            b.scale = UnityEngine.Random.Range(.5f, 1.5f);
+            b.waves = UnityEngine.Random.Range(4, 8);
             //UnityEngine.Debug.Log("Biome" + b.center.toString() + " --  Label " + b.label);
             return b;
         }
 
-        public float getDistance(Vector2 point) {
-            return Vector2.Distance(center, point);
+        public double getDistance(Vector2 point) {
+            return (Vector2.Distance(center, point)+0.85*Math.Sin(waves * Math.PI / 180f * Vector2.SignedAngle(center - point, Vector2.right))) * scale;
         }
     }
 
@@ -80,9 +84,15 @@ public class BoardManager : MonoBehaviour
                 GameObject t = Instantiate(tilePrefab, canvas.transform);
                 t.transform.SetPositionAndRotation(new Vector2(i * 30, j * 30), Quaternion.identity);
                 t.GetComponentInChildren<Text>().text = "" + bioM.getBiome(new Vector2(i, j)).label;
-                t.GetComponentInChildren<Text>().color = new Color(bioM.getBiome(new Vector2(i, j)).label/100f, 0, 0);
+                t.GetComponentInChildren<Text>().color = getColor(bioM.getBiome(new Vector2(i, j)).label);
             }
         }
+    }
+
+    static Color getColor(int i) {
+        if (i < 33) return new Color(i / 33f, 0, 0);
+        else if (i < 66) return new Color(0, (i - 33) / 33f, 0);
+        else return new Color(0, 0, (i - 66) / 34f);
     }
 
 
